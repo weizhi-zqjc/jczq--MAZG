e97aacfed8e8da0064577d9a60746eea21c6dca2
Lxg <85028732+Lxg6@users.noreply.github.com>
2021-07-07 21:56:55 +0800

    Add files via upload

---
+++ Task/dsjCOOKIE.js
@@ -0,0 +1,16 @@
+
+
+//独立COOKIE文件     ck在``里面填写，多账号换行
+let dsjheaderVal= `85f89cbfe427a3cdf1974d4fdd0f3083&WXpjMVltTmpaV1UyTURJek1qTXpNVGRrTlRRNVpqa3pOalEwTkRaaFlUST18MTYyNTY2MjI2OTQwNTIwOTMyNnw3ZDViM2QyMmQ0ZDE5OTVkZmQzOTJhYTU2NTFlOTZiYjkxMTMwODZh`
+let dsjurlVal= ``
+
+let dsjcookie = {
+  dsjheaderVal: dsjheaderVal,  
+  dsjurlVal: dsjurlVal,  
+
+}
+
+module.exports =  dsjcookie
+  
+
+
